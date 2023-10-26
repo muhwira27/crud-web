fda1764706d6ef2fb857eaa52a821c7ca2f24155
Muh. Wira <muhwira907@gmail.com>
2023-10-26 11:50:59 +0800

    Add student routes to express

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,10 @@
 import express from "express";
 import cors from "cors";
+import StudentRoute from "./routes/StudentRoute.js";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(StudentRoute);
 
 app.listen(5000, () => console.log('Server running on port 5000...'));
