b0adeba2a874c90f65ed2a67e33200824c64fd7d
Muh. Wira <muhwira907@gmail.com>
2023-10-26 11:53:43 +0800

    Add route for create, update, and delete student

--- backend/routes/StudentRoute.js
+++ backend/routes/StudentRoute.js
@@ -2,10 +2,16 @@ import express from "express";
 import {
   getStudents,
   getStudentById,
+  createStudent,
+  updateStudent,
+  deleteStudent
 } from "../controllers/StudentController.js";
 
 const router = express.Router();
 
 router.get('/students', getStudents);
 router.get('/students/:id', getStudentById);
+router.post('/students', createStudent);
+router.patch('/students/:id', updateStudent);
+router.delete('/students/:id', deleteStudent);
 export default router;
