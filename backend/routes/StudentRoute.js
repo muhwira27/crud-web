e83c444cda44f3dc471770c7d5ed701d12e577d6
Muh. Wira <muhwira907@gmail.com>
2023-10-26 11:51:57 +0800

    Add route for getStudentById controller

--- backend/routes/StudentRoute.js
+++ backend/routes/StudentRoute.js
@@ -1,9 +1,11 @@
 import express from "express";
 import {
   getStudents,
+  getStudentById,
 } from "../controllers/StudentController.js";
 
 const router = express.Router();
 
 router.get('/students', getStudents);
+router.get('/students/:id', getStudentById);
 export default router;
