78b3771741cc37e855bee21bf5eaa7a31b53e977
Muh. Wira <muhwira907@gmail.com>
2023-10-26 17:23:32 +0800

    refactor: implement contact list and add student component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import package
+import { useState } from "react";
+
+// import components
+import StudentList from "./components/StudentList";
+import Modal from "./components/AddStudentModal";
+
+// import styles
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [openModal, setOpenModal] = useState(false);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <main className="main">
+      <section className="title">
+        <h1>Daftar Mahasiswa</h1>
+        <div className="addStudentBtn">
+          <button onClick={() => setOpenModal(true)}>Tambah Mahasiswa</button>
+        </div>
+      </section>
+
+      {openModal && (
+        <Modal
+          close={() => setOpenModal(false)}
+        />
+      )}
+
+      <section className="container">
+        <StudentList />
+      </section>
+    </main>
+  );
 }
 
-export default App
+export default App;
